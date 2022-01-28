787a3393e98427636ab4c15ef369a5e199d226bd
ekimemre <eemre.ekim@gmail.com>
2022-01-28 11:30:57 +0300

    firestore mock and README.md added

--- src/__test__/AddTicket.test.js
+++ src/__test__/AddTicket.test.js
@@ -2,8 +2,28 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import AddTicket from '../components/AddTicket'
+import FirestoreMock from './firestore.mock'
+
+jest.mock('firebase/app', () => {
+  return {
+    auth: jest.fn(),
+  }
+})
+
+jest.mock('firebase/app', () => {
+  return {
+    auth: jest.fn().mockReturnThis(),
+    signInWithEmailAndPassword: jest.fn(),
+  }
+})
 
 describe('Rendering Landing Page Components', () => {
+  const firestoreMock = new FirestoreMock()
+  beforeEach(() => {
+    firebase.firestore = firestoreMock
+    firestoreMock.reset()
+  })
+
   const user = { username: 'user' }
   global.getAuth = jest.fn()
 
